3dbad935cd7f3ea42db609f79a0d9b229cecff4a
trungdang <pumaover@gmail.com>
2023-07-05 08:20:45 +0700

    [feat/add-client-page-ui] make ui for add client page

--- src/pages/agency/AddClient/Connections/Connections.tsx
+++ src/pages/agency/AddClient/Connections/Connections.tsx
@@ -2,15 +2,23 @@ import { Button, Space } from 'antd';
 import PlusIcon from 'assets/Icons/Plus';
 import { ConnectionItem } from './ConnectionItem';
 import classes from './Connections.module.css';
+import IntegrationDrawer from '../IntegrationDrawer/IntegrationDrawer';
+import { useState } from 'react';
 
 interface ConnectionsProps {
   step: any;
 }
 
 export const Connections = (props: ConnectionsProps) => {
+  const [openDraw, setOpenDraw] = useState<boolean>(false);
   const { step } = props;
   return (
     <>
+      <IntegrationDrawer
+        open={openDraw}
+        onClose={() => setOpenDraw(false)}
+        closable={false}
+      />
       <Space className={classes.connections_header}>
         <h1 className={classes.title}>{step.title}</h1>
         <Button
@@ -23,6 +31,7 @@ export const Connections = (props: ConnectionsProps) => {
             justifyContent: 'space-around',
             color: '#0062FF',
           }}
+          onClick={() => setOpenDraw(true)}
         >
           <b>New integration</b>
         </Button>
