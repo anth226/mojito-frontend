5f1b7c39a793e7f9809dadc31fed78c5b4731bc7
sharif Habiman <hdidiersharif@gmail.com>
2023-08-28 19:29:57 +0200

    fix URL

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,3 +1,5 @@
-export const APP_API_URL = 'http://localhost:1234/gql';
+export const APP_API_URL =
+  process.env.REACT_APP_API_URI ||
+  'https://mojito-server-production.up.railway.app/gql';
 
 export const STRIPE_PUBLIC_KEY='pk_test_51Mfw0UFvL0m5gmDoAfBKoxzxzmUhr2llJTGylhdGdfNUYfEDuaB7IAkyrcIwhrbZoCzu4BlwszKjPwZwkcrjzAre00cZMroCeE'
