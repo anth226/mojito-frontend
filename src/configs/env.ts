688c429d5515d1bf822654b02b5bee4f28b7687d
sharif Habiman <hdidiersharif@gmail.com>
2023-09-01 18:03:41 +0200

    fixing build

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,5 +1,5 @@
 export const APP_API_URL =
   process.env.REACT_APP_API_URI ||
-  'https://mojito-server-production.up.railway.app/gql';
+  'http://35.153.200.12/gql';
 
 export const STRIPE_PUBLIC_KEY='pk_test_51Mfw0UFvL0m5gmDoAfBKoxzxzmUhr2llJTGylhdGdfNUYfEDuaB7IAkyrcIwhrbZoCzu4BlwszKjPwZwkcrjzAre00cZMroCeE'
